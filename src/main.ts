88d1656057ffc32a524f6d168894398a647c0c78
Duong Mau <mauduong.dev@gmail.com>
2021-07-27 18:32:38 +0700

    Transform date with dayjs and class-tranformer

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { TransformResponseInterceptor } from './common/interceptors/transform-response.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalInterceptors(new TransformResponseInterceptor());
+
   await app.listen(3000);
 }
 bootstrap();
--- src/patients/patient.entity.ts
+++ src/patients/patient.entity.ts
@@ -3,7 +3,15 @@ import { SpecimenType } from 'src/common/enums/specimenType.enum';
 import { TestMethod } from 'src/common/enums/TestingMethod.enum';
 import { TestReason } from 'src/common/enums/TestReason.enum';
 import { TestResult } from 'src/common/enums/TestResult.enum';
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  BeforeInsert,
+  BeforeUpdate,
+  Column,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import * as dayjs from 'dayjs';
+import { Transform } from 'class-transformer';
 
 @Entity()
 export class Patient {
@@ -29,9 +37,15 @@ export class Patient {
   testReason: TestReason;
 
   @Column()
+  @Transform(({ value }) => dayjs(value).format('YYYYMMDDHHmm'), {
+    toPlainOnly: true,
+  })
   testDate: Date;
 
   @Column()
+  @Transform(({ value }) => dayjs(value).format('YYYYMMDDHHmm'), {
+    toPlainOnly: true,
+  })
   resultDate: Date;
 
   @Column({ default: null })
@@ -42,4 +56,16 @@ export class Patient {
 
   @Column()
   PCRValue: string;
+
+  @BeforeInsert()
+  parseDateInsert() {
+    this.testDate = dayjs(this.testDate, 'YYYYMMDDHHmm').toDate();
+    this.resultDate = dayjs(this.resultDate, 'YYYYMMDDHHmm').toDate();
+  }
+
+  @BeforeUpdate()
+  parseDateUpdate() {
+    this.testDate = dayjs(this.testDate, 'YYYYMMDDHHmm').toDate();
+    this.resultDate = dayjs(this.resultDate, 'YYYYMMDDHHmm').toDate();
+  }
 }
